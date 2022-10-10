9339c8357562c04846b4755e65d49cf0927f96fb
Sondre.Bolland1 <sondre.bolland@student.uib.no>
2022-10-10 13:20:57 +0200

    Add code

---
+++ src/main/java/student/ProblemSolver.java
@@ -0,0 +1,30 @@
+package student;
+
+import java.util.List;
+
+import graph.*;
+
+public class ProblemSolver implements IProblem {
+
+	@Override
+	public <V, E extends Comparable<E>> List<Edge<V>> mst(WeightedGraph<V, E> g) {
+		throw new UnsupportedOperationException();
+		// Task 1
+		// TODO: Implement me :)
+	}
+
+	@Override
+	public <V> V lca(Graph<V> g, V root, V u, V v) {
+		throw new UnsupportedOperationException();
+		// Task 2
+		// TODO: Implement me :)
+	}
+
+	@Override
+	public <V> Edge<V> addRedundant(Graph<V> g, V root) {
+		throw new UnsupportedOperationException();
+		// Task 3
+		// TODO: Implement me :)
+	}
+
+}
